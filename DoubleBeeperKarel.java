81dea4cda1faa28fd91a64727d7509aac1ef4091
Aditi Chamarthy <aditi.chamarthy@gmail.com>
2024-03-28 18:11:28 -0700

    Create DoubleBeeperKarel.java

---
+++ DoubleBeeperKarel.java
@@ -0,0 +1,43 @@
+/*
+ * File: DoubleBeeperKarel.java
+ * -----------------------------
+ * The DoubleBeeperKarel class extends the basic Karel class
+ * so that Karel doubles the number of beepers on a corner.
+ */
+
+import stanford.karel.*;
+
+public class DoubleBeeperKarel extends SuperKarel {
+
+	public void run() {
+		move();
+		doubleBeepers();
+		move();
+	}
+	
+	public void doubleBeepers() {
+		while(beepersPresent()){
+			pickBeeper();
+			move();
+			putBeeper();
+			putBeeper();
+			turnAround();
+			move();
+			turnAround();
+		}
+		move();
+		while(beepersPresent())
+		{
+			pickBeeper();
+			turnAround();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+		}
+		turnAround();
+		move();
+		turnAround();
+	}
+	    
+}
